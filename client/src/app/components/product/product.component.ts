97322e740364c0068c58e0104a1fb7d8c2007532
mahmudhmh <mahmoudh.buss@gmail.com>
2024-04-21 03:47:26 +0200

    feat: Enhance client-side items card with editable, deletable, and addable features

--- client/src/app/components/product/product.component.ts
+++ client/src/app/components/product/product.component.ts
@@ -1,19 +1,59 @@
 import { Product } from './../../../types';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService } from 'primeng/api';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule],
+  imports: [
+    RatingModule,
+    FormsModule,
+    ButtonModule,
+    ConfirmPopupModule,
+    ToastModule,
+  ],
+  providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
+  constructor(private confirmationService: ConfirmationService) {}
+  @ViewChild('deleteButton') deleteButton: any;
+
   @Input() product!: Product;
-  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+
+  @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+
+  deleteProduct() {
+    this.delete.emit(this.product);
+  }
+
+  editProduct() {
+    this.edit.emit(this.product);
+  }
+
+  confirmDelete() {
+    this.confirmationService.confirm({
+      target: this.deleteButton.nativeElement,
+      message: 'Are you sure that you want to delete this product?',
+      accept: () => {
+        this.deleteProduct();
+      },
+    });
+  }
   ngOnInit() {
-    this.productOutput.emit(this.product);
+    // this.productOutput.emit(this.product);
   }
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -2,13 +2,21 @@ import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../components/product/product.component';
 import { Product, Products } from './../../types';
 import { ProductsService } from './../services/products.service';
-import { Component } from '@angular/core';
-import { PaginatorModule } from 'primeng/paginator';
+import { Component, ViewChild } from '@angular/core';
+import { PaginatorModule, Paginator } from 'primeng/paginator';
+import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule, PaginatorModule],
+  imports: [
+    ProductComponent,
+    CommonModule,
+    PaginatorModule,
+    EditPopupComponent,
+    ButtonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -16,10 +24,53 @@ export class HomeComponent {
   //dependency injection but needs providers in app.module.ts
   constructor(private productsService: ProductsService) {}
 
+  @ViewChild('paginator') paginator: Paginator | undefined;
+
   products: Product[] = [];
 
   totalRecords: number = 0;
   rows: number = 5;
+
+  displayEditPopup: boolean = false;
+  displayAddPopup: boolean = false;
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditPopup = true;
+  }
+
+  toggleAddPopup() {
+    this.displayAddPopup = true;
+  }
+
+  toggleDeletePopup(product: Product) {
+    if (!product.id) {
+      return;
+    }
+    this.deleteProduct(product.id);
+  }
+
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0,
+  };
+
+  onConfirmEdit(product: Product) {
+    if (!this.selectedProduct.id) {
+      return;
+    }
+
+    this.editProduct(product, this.selectedProduct.id);
+    this.displayEditPopup = false;
+  }
+
+  onConfirmAdd(product: Product) {
+    this.addProduct(product);
+    this.displayAddPopup = false;
+  }
 
   onProductOutput(product: Product) {
     console.log(product, 'output');
@@ -29,12 +80,66 @@ export class HomeComponent {
     this.fetchProducts(event.page, event.rows);
   }
 
+  resetPaginator() {
+    this.paginator?.changePage(0);
+  }
+
   fetchProducts(page: number, perPage: number) {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page, perPage })
-      .subscribe((products: Products) => {
-        this.products = products.items;
-        this.totalRecords = products.total;
+      .subscribe({
+        next: (data: Products) => {
+          this.products = data.items;
+          this.totalRecords = data.total;
+        },
+        error: (error) => {
+          console.log('error', error);
+        },
+      });
+  }
+
+  editProduct(product: Product, id: number) {
+    this.productsService
+      .editProduct(`http://localhost:300/clothes/${id}`, product)
+      .subscribe({
+        next: (data) => {
+          console.log('product edited', data);
+          this.fetchProducts(0, this.rows);
+          this.resetPaginator();
+        },
+        error: (error) => {
+          console.log('error', error);
+        },
+      });
+  }
+
+  deleteProduct(id: number) {
+    this.productsService
+      .deleteProduct(`http://localhost:3000/clothes/${id}`)
+      .subscribe({
+        next: (data) => {
+          console.log('product deleted', data);
+          this.fetchProducts(0, this.rows);
+          this.resetPaginator();
+        },
+        error: (error) => {
+          console.log('error', error);
+        },
+      });
+  }
+
+  addProduct(product: Product) {
+    this.productsService
+      .addProduct(`http://localhost:3000/clothes`, product)
+      .subscribe({
+        next: (data) => {
+          console.log('product added', data);
+          this.fetchProducts(0, this.rows);
+          this.resetPaginator();
+        },
+        error: (error) => {
+          console.log('error', error);
+        },
       });
   }
 
